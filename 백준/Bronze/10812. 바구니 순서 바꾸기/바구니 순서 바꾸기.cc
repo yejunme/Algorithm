ccb7a72edbe3c7e760d5fb0bcc01d95410125347
Yejun Kim <132208002+yejunme@users.noreply.github.com>
2023-05-10 00:43:55 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
@@ -1,45 +1,33 @@
 #include <iostream>
 
-using namespace std;
+int main()
+{
+	int arr[101], brr[101];
+	int n, m;
+	int begin, mid, end;
 
-int main() {
+	std::cin >> n >> m;
 
-    int n, m;
-    
-    cin >> n >> m;
-    
-    int arr[101];
-    for (int i = 1; i <= 100; i++) {
-        arr[i] = i;
-    }
-    
-    int begin, end, mid;
-    for (int i = 0; i < m; i++) 
-    {
-        cin >> begin >> end>> mid;
-        
-        int start = begin;
-        int brr[101] = { 0 };
-        
-        for (int j = begin;j<mid;j++) 
-            brr[j] = arr[j]; 
-        
-        int cnt = 0;
-        for (int j = mid; j <= end; j++) 
-        {
-            arr[start] = arr[j];
-            start++;    
-        }
-       
-        for (int j = begin; j < mid; j++) 
-        {
-            arr[start] = brr[j];
-            start++;
-        }
-    }
+	for (int i = 1; i <= n; i++)
+		arr[i] = i;
 
-    for (int i = 1; i <= n; i++) 
-        cout << arr[i] << ' ';
-    
-    return 0;
+	for (int i = 0; i < m; i++)
+	{
+		std::cin >> begin >> end >> mid;
+
+		int start = begin;
+		for (int j = begin; j < mid; j++)
+			brr[j] = arr[j];
+
+		for (int j = mid; j <= end; j++)
+			arr[start++] = arr[j];
+
+		for (int j = begin; j < mid; j++)
+			arr[start++] = brr[j];
+	}
+
+	for (int i = 1; i <= n; i++)
+		std::cout << arr[i] << ' ';
+
+	return 0;
 }
