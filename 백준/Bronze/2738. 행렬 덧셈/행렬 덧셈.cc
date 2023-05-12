bfd7a93b579a4c279e0af28d6de5b9ab2f4b7926
Yejun Kim <132208002+yejunme@users.noreply.github.com>
2023-05-12 14:04:15 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -3,27 +3,26 @@
 int main()
 {
 	int arr[100][100] = { 0, };
-	int brr[100][100] = { 0, };
-	int n, m;
+	int n, m, tmp;
 
 	std::cin >> n >> m;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 2; i++)
 	{
-		for (int j = 0; j < m; j++)
-			std::cin >> arr[i][j];
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < m; k++)
+			{
+				std::cin >> tmp;
+				arr[j][k] += tmp;
+			}
+		}
 	}
 
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
-			std::cin >> brr[i][j];
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-			std::cout << arr[i][j] + brr[i][j] << ' ';
+			std::cout << arr[i][j] << ' ';
 		std::cout << std::endl;
 	}
 
